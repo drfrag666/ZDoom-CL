939da83e12a51d91ecd7d943d0611bcda6f5dfe3
Braden Obrzut <admin@maniacsvault.net>
2015-03-23 21:51:33 -0400

    - Fixed: Default movedown bind was set to "insert" and not "ins".
    (cherry picked from commit 44f9f2bbd626c3dea4a51b2fb04ba497e678d0ee)
    
    # Conflicts:
    #	src/c_bind.cpp

--- src/c_bind.cpp
+++ src/c_bind.cpp
@@ -125,7 +125,7 @@ static const FBinding DefBindings[] =
 static const FBinding DefRavenBindings[] =
 {
 	{ "pgup", "+moveup" },
-	{ "insert", "+movedown" },
+	{ "ins", "+movedown" },
 	{ "home", "land" },
 	{ "pgdn", "+lookup" },
 	{ "del", "+lookdown" },
