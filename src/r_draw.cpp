57b0ccb526a28349c4247d811bfd16e612bcc93c
drfrag <drfrag666@hotmail.com>
2017-10-27 15:00:49 +0200

    - Fixed offset for 3x2 and 4x4 low detail modes on some intel processors.
    # Conflicts:
    #	src/r_draw.cpp
    
    # Conflicts:
    #	src/r_draw.cpp

--- src/r_draw.cpp
+++ src/r_draw.cpp
@@ -1997,7 +1997,8 @@ void R_DetailDouble ()
 			int y,x;
 			BYTE *linefrom, *lineto;
 			BYTE c;
-			int offset = SCREENWIDTH > 640 ? CPU.DataL1LineSize : 0;
+			int pitchadj = CPU.bIsAMD ? CPU.DataL1LineSize : MAX(0, CPU.DataL1LineSize - 8);
+			int offset = SCREENWIDTH > 640 ? pitchadj : 0;
 
 			if (screenblocks <= 9)
 			{
@@ -2043,7 +2044,8 @@ void R_DetailDouble ()
 			int y,x;
 			BYTE *linefrom, *lineto;
 			BYTE c;
-			int offset = SCREENWIDTH > 640 ? CPU.DataL1LineSize*2 : 0;
+			int pitchadj = CPU.bIsAMD ? CPU.DataL1LineSize : MAX(0, CPU.DataL1LineSize - 8);
+			int offset = SCREENWIDTH > 640 ? pitchadj*2 : 0;
 
 			if (screenblocks <= 9)
 			{
