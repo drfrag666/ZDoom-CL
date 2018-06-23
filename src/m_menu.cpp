beaec980a8f679d9970a7ea1a3b41b8739579a97
drfrag666 <drfrag666@hotmail.com>
2018-06-23 19:13:29 +0200

    Revert "- Changed savegame list order, now they are sorted by slot number instead of alphabetically."
    This reverts commit 88778f62cc5074f9871c840f692e7d49b720d68b.
    
    Not in a classic version.
    
    # Conflicts:
    #	src/m_menu.cpp

--- src/m_menu.cpp
+++ src/m_menu.cpp
@@ -820,9 +820,8 @@ void M_InsertSaveNode (FSaveGameNode *node)
 	else
 	{ // Add node at top of list
 		probe = static_cast<FSaveGameNode *>(SaveGames.Head);
-		//while (probe->Succ != NULL && !probe->bOldVersion &&
-			//stricmp (node->Title, probe->Title) > 0)
-		while (probe->Succ != NULL && stricmp (node->Filename, probe->Filename) > 0)
+		while (probe->Succ != NULL && !probe->bOldVersion &&
+			stricmp (node->Title, probe->Title) > 0)
 		{
 			probe = static_cast<FSaveGameNode *>(probe->Succ);
 		}
